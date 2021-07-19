3dc1093f28813416dac80bceed8250b21fea8b20
rmzNadir <dgzrz99@gmail.com>
2021-07-19 10:33:56 -0500

    :lipstick: cleaned up sales dashboard code

--- src/Views/Sales/SalesHistory.tsx
+++ src/Views/Sales/SalesHistory.tsx
@@ -1,24 +1,75 @@
+import { useState, useCallback, useEffect, useRef } from 'react';
+import { message } from 'antd';
 import { IPagination, ISaleRecord } from '../../Types';
+import Amogus from '../../Utils/Amogus';
 import SalesTable from './SalesTable';
 import { HeaderSpace, Search } from './styles';
 
-interface Props {
-  handleSearchbar(value: string): void;
-  loading: boolean;
-  sales: ISaleRecord[];
-  setPaginationParams: React.Dispatch<React.SetStateAction<IPagination>>;
-  totalRecords: number;
-  paginationParams: IPagination;
-}
+const DEF_PAGINATION = {
+  per_page: 10,
+  page: 1,
+};
 
-const SalesHistory = ({
-  handleSearchbar,
-  loading,
-  sales,
-  setPaginationParams,
-  totalRecords,
-  paginationParams,
-}: Props) => {
+const SalesHistory = () => {
+  const [loading, setLoading] = useState(false);
+  const [sales, setSales] = useState<ISaleRecord[]>([]);
+  const [totalRecords, setTotalRecords] = useState<number>(0);
+  const [paginationParams, setPaginationParams] =
+    useState<IPagination>(DEF_PAGINATION);
+
+  const renders = useRef(0);
+  const setData = (salesdata: ISaleRecord[]) => {
+    setSales(salesdata);
+    setLoading(false);
+  };
+
+  const getProducts = useCallback(async () => {
+    setLoading(true);
+    try {
+      const params = new URLSearchParams(
+        paginationParams as unknown as Record<string, string>
+      );
+      const [res] = await Amogus(
+        {
+          method: 'GET',
+          url: `/sales/?${params}`,
+        },
+        false
+      );
+
+      const {
+        data,
+        headers: { total },
+      } = res;
+
+      // Yet another guetto workaround, in this case it helps avoid the ugly stuttering animation on 1st renders
+      if (renders.current <= 1) {
+        setTimeout(() => {
+          setData(data);
+        }, 250);
+      } else {
+        setData(data);
+      }
+
+      setTotalRecords(total);
+    } catch (e) {
+      setLoading(false);
+
+      message.error('Ocurrió un error al cargar el historial de ventas');
+    }
+  }, [paginationParams]);
+
+  useEffect(() => {
+    renders.current += 1;
+    getProducts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [paginationParams]);
+
+  const handleSearchbar = (value: string) => {
+    const trimmed = value.trim();
+    setPaginationParams((p) => ({ ...p, q: trimmed, page: 1 }));
+  };
+
   return (
     <>
       <HeaderSpace>
--- src/Views/Sales/index.tsx
+++ src/Views/Sales/index.tsx
@@ -1,9 +1,5 @@
-import { useState, useCallback, useEffect, useRef } from 'react';
-import { message, Tabs } from 'antd';
+import { Tabs } from 'antd';
 import Dashboard from '../../Components/Dashboard';
-
-import { IPagination, ISaleRecord } from '../../Types';
-import Amogus from '../../Utils/Amogus';
 import CollapseProvider from '../../Utils/CollapseContext';
 import SalesHistory from './SalesHistory';
 import SalesDashboard from './SalesDashboard';
@@ -11,91 +7,21 @@ import { useAuthCTX } from '../../Utils/AuthContext';
 
 const { TabPane } = Tabs;
 
-const DEF_PAGINATION = {
-  per_page: 10,
-  page: 1,
-};
-
 const Sales = () => {
-  const [loading, setLoading] = useState(false);
-  const [sales, setSales] = useState<ISaleRecord[]>([]);
-  const [totalRecords, setTotalRecords] = useState<number>(0);
-  const [paginationParams, setPaginationParams] =
-    useState<IPagination>(DEF_PAGINATION);
-
-  const renders = useRef(0);
   const { isAdmin } = useAuthCTX();
-
-  const setData = (salesdata: ISaleRecord[]) => {
-    setSales(salesdata);
-    setLoading(false);
-  };
-
-  const getProducts = useCallback(async () => {
-    setLoading(true);
-    try {
-      const params = new URLSearchParams(
-        paginationParams as unknown as Record<string, string>
-      );
-      const [res] = await Amogus(
-        {
-          method: 'GET',
-          url: `/sales/?${params}`,
-        },
-        false
-      );
-
-      const {
-        data,
-        headers: { total },
-      } = res;
-
-      // Yet another guetto workaround, in this case it helps avoid the ugly stuttering animation on 1st renders
-      if (renders.current <= 1) {
-        setTimeout(() => {
-          setData(data);
-        }, 250);
-      } else {
-        setData(data);
-      }
-
-      setTotalRecords(total);
-    } catch (e) {
-      setLoading(false);
-
-      message.error('Ocurrió un error al cargar el historial de ventas');
-    }
-  }, [paginationParams]);
-
-  useEffect(() => {
-    renders.current += 1;
-    getProducts();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [paginationParams]);
-
-  const handleSearchbar = (value: string) => {
-    const trimmed = value.trim();
-    setPaginationParams((p) => ({ ...p, q: trimmed, page: 1 }));
-  };
 
   return (
     <CollapseProvider>
       <Dashboard selectedKeys='sales' sectionName='Ventas' clientView>
-        <Tabs defaultActiveKey={isAdmin ? 'dashboard' : 'table'}>
+        {/* {isAdmin ? 'dashboard' : 'table'} */}
+        <Tabs defaultActiveKey='table'>
           {isAdmin && (
             <TabPane tab='Dashboard' key='dashboard'>
               <SalesDashboard />
             </TabPane>
           )}
           <TabPane tab='Historial' key='table'>
-            <SalesHistory
-              setPaginationParams={setPaginationParams}
-              paginationParams={paginationParams}
-              loading={loading}
-              handleSearchbar={handleSearchbar}
-              totalRecords={totalRecords}
-              sales={sales}
-            />
+            <SalesHistory />
           </TabPane>
         </Tabs>
       </Dashboard>
