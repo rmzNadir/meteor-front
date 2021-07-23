f64d5b138985c2672a8f13d1bddae25317e43de4
rmzNadir <dgzrz99@gmail.com>
2021-07-22 22:35:21 -0500

    :bug: Fixed product id url not opening details when user isn't logged in

--- src/Components/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute.tsx
@@ -1,5 +1,6 @@
 import { Route, Redirect } from 'react-router-dom';
 import { useAuthCTX } from '../Utils/AuthContext';
+import GetQueryParams from '../Utils/GetQueryParams';
 
 interface Props {
   exact?: boolean;
@@ -9,11 +10,14 @@ interface Props {
 
 const ProtectedRoute = ({ component, exact = false, path }: Props) => {
   const { isAuth } = useAuthCTX();
+  const id = GetQueryParams('id');
+
   if (isAuth) {
     return <Route exact={exact} path={path} component={component} />;
   }
 
-  return <Redirect to='/' />;
+  // Add ID to redirect url in case user opened a link to /listings?id=<id> and wasn't logged in
+  return <Redirect to={id ? `/?id=${id}` : '/'} />;
 };
 
 export default ProtectedRoute;
